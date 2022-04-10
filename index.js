be7df43e27347bcce866f87ec9f14c6d93be72d9
Dom <Domalbano35@gmail.com>
2022-04-10 14:47:19 -0400

    on completion of prompt fields, a dynamic card generates with employee info

--- index.js
+++ index.js
@@ -60,27 +60,55 @@ const questions = [
 ];
 
 function generateHTML(answers) {
-  return `  <div class="card" style="width: 18rem;">
-    
+  return `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Profile Generator</title>
+  <link rel="stylesheet" href="/dist/style.css" />
+  <!-- CSS only -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+</head>
+
+<body>
+  <nav class="my-navbar navbar navbar-dark bg-danger">
+    <div class="container-fluid justify-content-center">
+      <span class="navbar-brand mb-0 h1 header ">My Team</span>
+    </div>
+  </nav>
+
+
+  <div class="card" style="width: 18rem;">
     <div class="card-body bg-primary">
-      <h5 class="card-title text-light">${questions.name}</h5>
-      <p class="card-text text-light">${questions.employeeTitle}</p>
+      <h5 class="card-title text-light">${answers.name}</h5>
+      <p class="card-text text-light">Position: ${answers.employeeTitle}</p>
     </div>
     <ul class="list-group list-group-flush">
-      <li class="list-group-item">${questions.iD}</li>
-      <li class="list-group-item">${questions.email}</li>
-      <li class="list-group-item">${questions.officeNumber}</li>
+      <li class="list-group-item">Office Number: ${answers.iD}</li>
+      <li class="list-group-item">${answers.email}</li>
+      <li class="list-group-item">${answers.officeNumber}, ${answers.gitHub}</li>
     </ul>
+  </div>
 
-  </div>`
+
+
+
+</body>
+
+</html>`;
 };
+
 
 
 function init() {
   inquirer.prompt(questions).then((answers) => {
     console.log(answers);
 //fix whatever is making this make new html file
-    fs.appendFileSync("index.html", generateHTML(answers), (err) => {
+    fs.writeFileSync("index.html", generateHTML(answers), (err) => {
       if (err) {
         console.log(err);
         return;
@@ -92,18 +120,36 @@ function init() {
 }
 
 init();
+
+
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for my team members and their information
 // THEN an HTML file is generated that displays a nicely formatted team roster based on user input
+
+
 // WHEN I click on an email address in the HTML
 // THEN my default email program opens and populates the TO field of the email with the address
+
+
 // WHEN I click on the GitHub username
 // THEN that GitHub profile opens in a new tab
+
+
 // WHEN I start the application
 // THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+
+
 // WHEN I enter the team manager’s name, employee ID, email address, and office number
 // THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
+
+
 // WHEN I select the engineer option
 // THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
+
+
 // WHEN I select the intern option
 // THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
+
+
 // WHEN I decide to finish building my team
 // THEN I exit the application, and the HTML is generated
