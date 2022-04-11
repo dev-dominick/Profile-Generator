45689b096c03b25680617b0a74bd04211295d3c1
Dom <Domalbano35@gmail.com>
2022-04-10 21:29:53 -0400

    generatecard.js updates

--- src/generateCards.js
+++ src/generateCards.js
@@ -2,7 +2,7 @@
 const cardManager = function (manager) {
   return `  <div class="card" style="width: 18rem;">
     <div class="card-body bg-primary">
-      <h5 class="card-title text-light">${manager.name}</h5>
+      <h5 class="card-title text-light">${manager.getname}</h5>
       <p class="card-text text-light">Manager</p>
     </div>
     <ul class="list-group list-group-flush">
@@ -12,3 +12,50 @@ const cardManager = function (manager) {
     </ul>
   </div>`;
 }
+
+const teamBuilder = (employee) => {
+  cardArray = [];
+  if (role = "manager") {
+    var managerCard = cardManager(employee)
+    console.log(LockManager);
+  }
+  cardArray.push(managerCard);
+  console.log(managerCard);
+  const buildTeam = generateHTML(cardManager);
+  return buildTeam
+}
+
+function generateHTML(manager) {
+  return `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Profile Generator</title>
+  <link rel="stylesheet" href="/dist/style.css" />
+  <!-- CSS only -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+</head>
+
+<body>
+  <nav class="my-navbar navbar navbar-dark bg-danger">
+    <div class="container-fluid justify-content-center">
+      <span class="navbar-brand mb-0 h1 header ">My Team</span>
+    </div>
+  </nav>
+
+${cardManager}
+
+
+
+
+
+</body>
+
+</html>`;
+};
+
+module.exports = generateHTML;
